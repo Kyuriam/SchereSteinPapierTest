94fe5da4d6cc189161ea7184641b9f8486eb2cf3
TimoW <timo.winzer18@gmail.com>
2021-02-20 15:42:08 +0100

    Ausgelagerte Klassen

--- SchereSteinPapierEchseSpock/Program.cs
+++ SchereSteinPapierEchseSpock/Program.cs
@@ -4,13 +4,17 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZeichenKlasse;
 
 namespace SchereSteinPapierEchseSpock
 {
     class Program
     {
-        static void Main(string[] args)
+        // Die Methode für eine Runde Schere Stein Papier Echse Spock
+        // Rekursion bei Wiederspiel Bedarf des Spielers
+        public void spiel(string a)
         {
+            string name = a;
             // Alle Zeichen die mitspielen sollen werden initialisiert
             Zeichen Schere = new Zeichen("Schere", new string[] { "Papier", "Echse" });
             Zeichen Stein  = new Zeichen("Stein" , new string[] { "Schere", "Echse" });
@@ -23,93 +27,62 @@ namespace SchereSteinPapierEchseSpock
             Zeichen eigenesZeichen;
             Zeichen[] alleZeichen = new Zeichen[] { Schere, Stein, Papier, Echse, Spock };
 
+            // Spielerzeichen wählen
+            Console.WriteLine("Bitte wählen sie ihr Zeichen als Zahl. Möglich sind: ");
+            Console.WriteLine(" 0:Schere | 1:Stein | 2:Papier | 3:Echse | 4:Spock");
+            Console.WriteLine(" Auswahl bestätigen mit Enter.");
+            string auswahlString = Console.ReadLine();
+            int auswahlZahl = Int32.Parse(auswahlString);
+            eigenesZeichen = alleZeichen[auswahlZahl];
+                
+            // Gegnerzeichen wird ein zufälliges Zeichen zugewiesen
+            gegnerZeichen = alleZeichen[zufallszahl(0,4)];
+                    
+            // Die Zeichen spielen gegeneinander
+            // Unentschieden ( gleiche Zeichen )
+            if (eigenesZeichen.getName() == gegnerZeichen.getName())
+            {
+                Console.WriteLine("Unentschieden! " + eigenesZeichen.getName() + " und " + gegnerZeichen.getName() + " sind gleiche Zeichen.");
+            } 
+                // Eigenes Zeichen gewinnt
+                if (eigenesZeichen.besiegtZeichen(gegnerZeichen))
+                {
+                    Console.WriteLine( name + " hat gewonnen! " + eigenesZeichen.getName() + " schlägt " + gegnerZeichen.getName() + ".");
+                }
+
+                // Gegnerisches Zeichen gewinnt
+                if (gegnerZeichen.besiegtZeichen(eigenesZeichen))
+                {
+                    Console.WriteLine( name + " hat verloren! " + gegnerZeichen.getName() + " schlägt " + eigenesZeichen.getName() + ".");
+                }
+
+                // Spiel wiederholen bei Bedarf
+                Console.WriteLine("Möchten sie noch eine Runde spielen, dann schreiben Sie 'ja' und bestätigen mit Enter");
+                if (Console.ReadLine() == "ja")
+                {
+                    spiel(name);
+                }
+        }
+
+        // Zufällige Zahl zwischen min-max wird erzeugt
+        private int zufallszahl(int min, int max)
+        {
+            Random zahl = new Random();
+            int ergebnis = (int)zahl.Next( min , max++);
+            return ergebnis;
+        }
+
+        static void Main(string[] args)
+        {
             // Spielernamen setzen
             Console.WriteLine("Bitte geben sie jetzt ihren Spielernamen ein, bestätigen sie mit Enter:");
-            string spielerName= Console.ReadLine();
+            string spielerName = Console.ReadLine();
+
+            Program testSpiel = new Program();
 
             // Einleitung des eigentlichen Spiels
-            spiel();
-
-                // Die Methode für eine Runde Schere Stein Papier Echse Spock
-                // Rekursion bei Wiederspiel Bedarf des Spielers
-                void spiel()
-                {
-                    // Spielerzeichen wählen
-                    Console.WriteLine("Bitte wählen sie ihr Zeichen als Zahl. Möglich sind: ");
-                    Console.WriteLine(" 0:Schere | 1:Stein | 2:Papier | 3:Echse | 4:Spock");
-                    string auswahlString = Console.ReadLine();
-                    int auswahlZahl = Int32.Parse(auswahlString);
-                    eigenesZeichen = alleZeichen[auswahlZahl];
-                
-                    // Gegnerzeichen wird ein zufälliges Zeichen zugewiesen
-                    gegnerZeichen = alleZeichen[zufallszahl(0,4)];
-                    
-                    // Die Zeichen spielen gegeneinander
-                    // Unentschieden ( gleiche Zeichen )
-                    if (eigenesZeichen.getName() == gegnerZeichen.getName())
-                    {
-                         Console.WriteLine("Unentschieden! " + eigenesZeichen.getName() + " und sind gleiche Zeichen " + gegnerZeichen.getName() + ".");
-                    } 
-                    // Eigenes Zeichen gewinnt
-                    if (eigenesZeichen.besiegtZeichen(gegnerZeichen))
-                    {
-                        Console.WriteLine( spielerName + " hat gewonnen! " + eigenesZeichen.getName() + " schlägt " + gegnerZeichen.getName() + ".");
-                    }
-                    // Gegnerisches Zeichen gewinnt
-                    if (gegnerZeichen.besiegtZeichen(eigenesZeichen))
-                    {
-                        Console.WriteLine("Sie haben verloren! " + gegnerZeichen.getName() + " schlägt " + eigenesZeichen.getName() + ".");
-                    }
-
-                    // Spiel wiederholen bei Bedarf
-                    Console.WriteLine("Möchten sie noch eine Runde spielen, dann schreiben Sie 'ja'");
-                    if (Console.ReadLine() == "ja")
-                    {
-                        spiel();
-                    }
-                }
-
-                // Zufällige Zahl zwischen min-max wird erzeugt
-                int zufallszahl(int min, int max)
-                {
-                    Random zahl = new Random();
-                    int ergebnis = (int)zahl.Next( min , max++);
-                    return ergebnis;
-                }
-
+            testSpiel.spiel(spielerName);
         }
 
-        // Die Klasse Zeichen sind die jeweiligen Handzeichen,
-        // mit dem string name und dem string array besiegt ( alle Zeichen gegen das jeweilige Zeichen gewinnt )
-        public class Zeichen
-        {
-            string name;
-            string[] besiegt;
-
-            public Zeichen(string a, string[] b)
-            {
-                this.name = a;
-                this.besiegt = b;
-            }
-
-            // Gibt einen Wahrheitswert wieder,
-            // je nachdem ob 'this' Zeichen gegen KontrahentZeichen gewinnt (true) oder verliert (false)
-            public bool besiegtZeichen(Zeichen KontrahentZeichen)
-            {
-                return this.besiegt.Contains(KontrahentZeichen.getName());
-            }
-            
-            // Gibt den Namen des Zeichens aus
-            public string getName()
-            {
-                return this.name;
-            }
-               
-            // Gibt den array mit den Namen gegen den das Zeichen gewinnt aus
-            public string[] getBesiegt()
-            {
-                return this.besiegt;
-            }
-        }
     }
 }
---
+++ SchereSteinPapierEchseSpock/Zeichen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeichenKlasse
+{
+    // Die Klasse Zeichen sind die jeweiligen Handzeichen,
+    // mit dem string name und dem string array besiegt ( alle Zeichen gegen das jeweilige Zeichen gewinnt )
+    public class Zeichen
+    {
+        string name;
+        string[] besiegt;
+
+        public Zeichen(string a, string[] b)
+        {
+            this.name = a;
+            this.besiegt = b;
+        }
+
+        // Gibt einen Wahrheitswert wieder,
+        // je nachdem ob 'this' Zeichen gegen KontrahentZeichen gewinnt (true) oder verliert (false)
+        public bool besiegtZeichen(Zeichen KontrahentZeichen)
+        {
+            return this.besiegt.Contains(KontrahentZeichen.getName());
+        }
+
+        // Gibt den Namen des Zeichens aus
+        public string getName()
+        {
+            return this.name;
+        }
+
+        // Gibt den array mit den Namen gegen den das Zeichen gewinnt aus
+        public string[] getBesiegt()
+        {
+            return this.besiegt;
+        }
+    }
+}
